bb0ac648a64dd18f54d2ba867bf84e91ed5d646d
see.rohit525 <see.rohit525@gmail.com>
2022-01-06 17:33:38 +0530

    changes in navbar

---
+++ appMobile.js
@@ -0,0 +1,46 @@
+
+var el = document.querySelector('#imgIcon')
+console.log(el)
+
+
+// el.addEventListener('click', myFunction)
+
+
+
+
+function myFunction() {
+    var element = document.getElementById("mobile");
+    element.classList.toggle("mystyle");
+}
+
+
+
+
+
+
+
+
+
+
+// function myfunc() {
+//     var parent = document.getElementsByClassName('.mobile')
+
+//     if (parent.style.display === "block") {
+//         parent.style.display = "none";
+
+//     } else {
+//         parent.style.display = "block"
+//     }
+
+// }
+
+
+// function myfunc() {
+
+//     var parent = document.getElementsByClassName('.mobile')
+
+//     console.log(parent)
+
+
+
+// }
